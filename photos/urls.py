1bdaa8f42b46f30dfa8eb28212203ed71409c037
Cencious <user.email>
2022-05-29 18:42:21 +0300

    create a url pattern for viewphoto and addphoto

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.gallery, name='gallery'),
-    
+    path('photo/<str:pk>/',views.viewPhoto, name='photo'),
+    path('add/',views.addPhoto,name='add'),
     ]
