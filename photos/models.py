bf7dbde4231cf8a905863305c1599a25702d693c
Cencious <user.email>
2022-05-29 21:15:54 +0300

    create models that will be migrated to db

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+
+class category(models.Model):
+    name= models.CharField(max_length=100,null=False, blank=False)
+
+    def __str__(self):
+        return self.name
