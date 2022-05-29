cd28f296d156ddb7fda853392b430b14452f06c6
Cencious <user.email>
2022-05-29 23:49:35 +0300

    delete the auto imports

--- photos/models.py
+++ photos/models.py
@@ -1,21 +1,21 @@
-from argparse import ONE_OR_MORE
-from turtle import onclick
-from unicodedata import name
 from django.db import models
 
 # Create your models here.
 
 
-class category(models.Model):
+class Category(models.Model):
     name= models.CharField(max_length=100,null=False, blank=False)
 
     def __str__(self):
         return self.name
 
-class photo(models.Model):
-   category= models.ForeignKey(category, on_delete=models.SET_NULL, null=True, blank=True)
+class Photo(models.Model):
+   category= models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
    
    image= models.ImageField(null=False, blank=False)
-   des
+   description = models.TextField()
+
+   def __str__(self):
+       return self.description
 
   
