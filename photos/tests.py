6af3d33b6437faa9915374e6cca61d126e7083bd
Cencious <user.email>
2022-05-30 20:42:00 +0300

    test functions

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Location,Category
+import datetime as dt
 
 # Create your tests here.
+
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Turkana = Location(location='Turkana')
+        self.Turkana.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Turkana,Location))
+    
+    def test_updating_location(self):
+        location = Location.get_location_id(self.Kisumu.id)
+        location.update_location('Kisumu')
+        location = Location.get_location_id(self.Kericho.id)
+        self.assertTrue(location.location == 'Kericho')
+    
+    def tearDown(self):
+        self.Kisumu.delete_location() 
+
+        
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.food = Category(category='')
+        self.food.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+    
+    def tearDown(self):
+        self.food.delete_category()
+
+
