b113dd20e464ce2c553d86422a8d8aa20d75ced3
Cencious <user.email>
2022-05-30 20:44:59 +0300

    add spacing

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.gallery, name='gallery'),
     path('photo/<str:pk>/',views.viewPhoto, name='photo'),
-    path('add/',views.addPhoto, name='add'),
+    path('add/',views.addPhoto, name= 'add'),
     ]
