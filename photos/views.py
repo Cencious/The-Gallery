1e066a0d26189bc7561f06e1397b4f54b09d9acb
Cencious <user.email>
2022-05-30 10:38:15 +0300

    query the data

--- photos/views.py
+++ photos/views.py
@@ -26,8 +26,20 @@ def addPhoto(request):
         data = request.POST
         image = request.FILES.get('image')
 
-        print('data:', data)
-        print('image:', image)
+        # print('data:', data)
+        # print('image:', image)
+        if data['category'] == 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] == '':
+            category,created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+        
+        Photo = Photo.objects.create(
+            category=category,
+            description=data['description']
+            image = image
+        )
 
     context = {'categories':categories,}
     return render(request, 'photos/add.html', context) 
