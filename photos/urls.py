214885aeee021fd61dcb3d6378030b7f420a97ed
Cencious <user.email>
2022-06-02 00:26:08 +0300

    add search path

--- photos/urls.py
+++ photos/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',views.gallery, name='gallery'),
     path('photo/<str:pk>/',views.viewPhoto, name='photo'),
     path('add/',views.addPhoto, name= 'add'),
+    path('search', views.searchPhoto, name='search'),
     ]
