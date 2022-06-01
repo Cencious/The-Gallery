77cd24b5e40f7a0c1676b9d502b8d2784e3584e2
Cencious <user.email>
2022-06-01 12:50:06 +0300

    spacing

--- photos/models.py
+++ photos/models.py
@@ -44,11 +44,8 @@ class Location(models.Model):
 class Photo(models.Model):
    category= models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
-
    image= models.ImageField(null=False, blank=False)
-   description = models.TextField()
-
-   
+   description = models.TextField()   
 #    pub_date = models.DateTimeField(auto_now_add=True, null=True) 
 
    def __str__(self):
